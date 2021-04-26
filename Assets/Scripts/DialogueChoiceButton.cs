9084512c6bc52606e20629fd44916d37fd461f17
Noor Mahini <nmahini@berkeley.edu>
2021-04-26 01:42:01 -0700

    on hover not working

---
+++ Assets/Scripts/DialogueChoiceButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class DialogueChoiceButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("pointer in");
+        //this.GetComponent<SpriteRenderer>().color = Color.blue;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("pointer out");
+        //this.GetComponent<SpriteRenderer>().color = Color.green;
+    }
+
+
+    public void makeChoice()
+    {
+
+    }
+}
--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,20 +1,93 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine.SceneManagement;using TMPro;using UnityEngine;public class DialogueManager : MonoBehaviour{    private FrameLoader frameLoader;    private Frame currentFrame;    private ImageLoader imageLoader;    private static DialogueManager _instance;    private TextMeshPro nameBox;    private TextMeshPro textBox;    [SerializeField]    private SpriteRenderer portrait;    public static DialogueManager Instance { get { return _instance; } }    private void Awake()    {        // Ensure singleton behavior        if (_instance != null && _instance != this)        {            Destroy(this.gameObject);        }        else        {            _instance = this;        }        frameLoader = new FrameLoader();        imageLoader = new ImageLoader();        frameLoader.Load();        imageLoader.Load();        startDialogue(SceneManager.GetActiveScene().name);    }    public void Update()
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private FrameLoader frameLoader;
+    private Frame currentFrame;
+
+    private ImageLoader imageLoader;
+
+    private static DialogueManager _instance;
+    private GameObject dialogueBox;
+    private GameObject choiceButtons;
+    private TextMeshPro nameBox;
+    private TextMeshPro textBox;
+
+    private SpriteRenderer portrait;
+    public static DialogueManager Instance { get { return _instance; } }
+    private void Awake()
+    {
+        // Ensure singleton behavior
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        dialogueBox = GameObject.Find("Dialogue Box");
+        choiceButtons = GameObject.Find("Choice Buttons");
+
+        portrait = GameObject.Find("Portrait").GetComponent<SpriteRenderer>();
+        textBox = GameObject.Find("Dialogue Text").GetComponent<TextMeshPro>();
+        nameBox = GameObject.Find("Name Box").GetComponent<TextMeshPro>();
+
+        frameLoader = new FrameLoader();
+        imageLoader = new ImageLoader();
+        frameLoader.Load();
+        imageLoader.Load();
+
+        //temporary
+        startDialogue(SceneManager.GetActiveScene().name);
+    }
+
+    public void Update()
     {
         if (Input.GetMouseButtonDown(0) && currentFrame.choices.Length == 1)
         {
             advanceFrame();
         }
-    }    private void triggerDialogue(string characterName)    {    }    private void startDialogue(string id)    {        currentFrame = frameLoader.getFrame(id);
-        textBox = gameObject.GetComponentsInChildren<TextMeshPro>()[0];        textBox.text = currentFrame.lines;        nameBox = gameObject.GetComponentsInChildren<TextMeshPro>()[1];        nameBox.text = currentFrame.name;        updateSprite();    }    private void advanceFrame()
+    }
+
+    /*
+        starts a new dialogue given a frame id
+    */
+    private void startDialogue(string id)
     {
-        updateFrame("default");
+        currentFrame = frameLoader.getFrame(id);
         textBox.text = currentFrame.lines;
         nameBox.text = currentFrame.name;
         updateSprite();
-    }    private void updateFrame(string path)
+    }
+
+    /*
+    */
+    private void advanceFrame()
     {
-        currentFrame = frameLoader.getFrame(currentFrame.choiceMappings[path]);
-    }    private void updateSprite()
+        setNextFrame("default");
+        textBox.text = currentFrame.lines;
+        nameBox.text = currentFrame.name;
+        updateSprite();
+    }
+
+    private void setNextFrame(string choice)
+    {
+        currentFrame = frameLoader.getFrame(currentFrame.choiceMappings[choice]);
+
+    }
+
+    private void updateSprite()
     {
         portrait.sprite = imageLoader.getSprite(currentFrame.name + "_" + currentFrame.portrait);
-    }}
+    }
+
+    private void setDialogueVisibility(bool isVisible)
+    {
+        dialogueBox.SetActive(isVisible);
+    }
+}
