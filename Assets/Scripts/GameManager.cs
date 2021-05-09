b6e3e2eb7774d3201f0e2b5d36b43d781b0c50dd
Noor Mahini <nmahini@berkeley.edu>
2021-05-09 14:42:22 -0700

    basic setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = null;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+    public void EnterDialogueScene()
+    {
+        SceneManager.LoadScene("xavier1_1");
+    }
+    public void EnterEndScene()
+    {
+        SceneManager.LoadScene("EndScene");
+    }
+}
